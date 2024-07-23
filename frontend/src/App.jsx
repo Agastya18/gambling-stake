48628bef6133473bd8f541853c112ec94bcb4b2e
agastya gaur <agastyagaur17@gmail.com>
2024-07-23 20:08:26 +0530

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='underline'>
+      <h1>asadad</h1>
+    </div>
+  )
+}
+
+export default App
